eb4dfb9a7f9830e29cf021d90c29b73639e8e2ee
Jonathon Buhler <jonathonbuhler@dhcp-10-5-91-116.ad.uvu.edu>
2025-02-04 14:22:07 -0700

    Updated footer and added some different styling accross website

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,10 +1,20 @@
+import styles from "./Footer.module.css";
+
 function Footer() {
   return (
-    <>
-      <br />
-      <br />
-      <br />
-    </>
+    <div className={styles.footer}>
+      <p>Thank you for celebrating with us! ❤️</p>
+      <p>
+        Made with love by Emmalyn and Jonathon | &copy;{" "}
+        {new Date().getFullYear()}
+      </p>
+      <p>
+        Contact us:{" "}
+        <a href="jonathonbuhler03@gmail.com">jonathonbuhler03@gmail.com</a>
+        <br />
+        <a href="emmalynhansen0@gmail.com">emmalynhansen0@gmail.com</a>
+      </p>
+    </div>
   );
 }
 
--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -2,29 +2,29 @@ import styles from "./Home.module.css";
 import us from "../../assets/us.jpg";
 
 function Home() {
-  const wedding_schedule = [
-    { label: "Start Time", time: "6:30" },
-    { label: "Line", time: "6:30-8:00" },
-    { label: "First Dances", time: "8:00-8:30" },
-    { label: "Cut Cake", time: "8:30-8:45" },
-    { label: "Send Off", time: "9:00" },
-  ];
-
   return (
     <section className={`grid ${styles["home-container"]}`}>
-      <div className={styles["wedding-details"]}>
-        <h2>The Wedding (Reception)</h2>
-        <hr />
-        <ul className={styles.schedule}>
-          {wedding_schedule.map((item, index) => (
-            <li key={index}>
-              <span className={styles.label}>{item.label}</span>
-              <span className={styles.time}>{item.time}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
       <img className={styles.us} src={us} alt="Us" />
+      <hr />
+      <div>
+        <h2>Wedding Day</h2>
+        <h3>May 10, 2025</h3>
+        <a
+          className={styles.manti}
+          href="https://maps.app.goo.gl/GHC86YA8Mm3kV1MH8"
+          target="_blank"
+        >
+          <address>
+            Manti Temple <i className="fa-solid fa-location-dot"></i>
+          </address>
+        </a>
+        <address>10:00 AM</address>
+      </div>
+      <hr />
+      <div>
+        <h2>Reception</h2>
+      </div>
+      <hr />
     </section>
   );
 }
